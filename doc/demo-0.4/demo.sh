3079d005fc5582fd63065ca7f1cc98c623935fa0
Nicola Busanello <nicola.busanello@gmail.com>
2022-05-16 15:19:06 +0200

    issue to 2 utxos + 1st transfer with 2 inputs

--- doc/demo-0.4/demo.sh
+++ doc/demo-0.4/demo.sh
@@ -83,7 +83,13 @@ gen_utxo() {
 
 issue_asset() {
     _subtit 'issuing asset'
-    _trace $RGB0 fungible issue USDT "USD Tether" 1000@$txid_issue:$vout_issue
+    gen_utxo issuer
+    txid_issue=$txid
+    vout_issue=$vout
+    gen_utxo issuer
+    txid_issue_2=$txid
+    vout_issue_2=$vout
+    _trace $RGB0 fungible issue USDT "USD Tether" 1000@$txid_issue:$vout_issue 1000@$txid_issue_2:$vout_issue_2
 }
 
 get_asset_id() {
@@ -105,6 +111,8 @@ transfer_asset() {
     local num="$9"              # transfer number
     local amt_send="${10}"      # asset amount to send
     local amt_change="${11}"    # asset amount to get back as change
+    local txid_send_2="${12}"   # sender txid n. 2
+    local vout_send_2="${13}"   # sender vout n. 2
 
     ## generate utxo to receive assets
     gen_utxo $rcpt_wlt
@@ -126,26 +134,42 @@ transfer_asset() {
     [ "$DEBUG" != 0 ] && _trace $BCLI -rpcwallet=$send_wlt listunspent
     local filter=".[] |select(.txid == \"$txid_send\") |.amount"
     local amnt="$(_trace $BCLI -rpcwallet=$send_wlt listunspent |tr -d '\r' |jq -r "$filter")"
+    if [ -n "$txid_send_2" -a -n "$vout_send_2" ]; then  # handle double input case
+        filter=".[] |select(.txid == \"$txid_send_2\") |.amount"
+        local amnt_2="$(_trace $BCLI -rpcwallet=$send_wlt listunspent |tr -d '\r' |jq -r "$filter")"
+        amnt=$(($amnt + $amnt_2))
+    fi
     local psbt=tx${num}.psbt
     local cons=consignment${num}.rgb
     local disc=discolsure${num}.rgb
     local wtns=witness${num}.psbt
-    local in="[{\"txid\": \"$txid_send\", \"vout\": $vout_send}]"
+    local in="["
+    in="${in} {\"txid\": \"$txid_send\", \"vout\": $vout_send}"
+    if [ -n "$txid_send_2" -a -n "$vout_send_2" ]; then  # handle double input case
+        in="${in}, {\"txid\": \"$txid_send_2\", \"vout\": $vout_send_2}"
+    fi
+    in="${in} ]"
     local out="[{\"$addr_send\": \"$amnt\"}]"
     local opts="{\"subtractFeeFromOutputs\": [0]}"
     _trace $BCLI -rpcwallet=$send_wlt walletcreatefundedpsbt "$in" "$out" 0 "$opts" \
         | jq -r '.psbt' | base64 -d > $send_data/$psbt
     if [ "$DEBUG" != 0 ]; then
+        _subtit "showing inputs from psbt"
+        _trace $BCLI decodepsbt $(base64 -w0 $send_data/$psbt) |tr -d '\r' |jq '.tx | .vin'
         _subtit "showing outputs from psbt"
         _trace $BCLI decodepsbt $(base64 -w0 $send_data/$psbt) |tr -d '\r' |jq '.outputs'
     fi
     sleep 1
     ## transfer
     _subtit "transferring asset"
+    local input="-i $txid_send:$vout_send"
+    if [ -n "$txid_send_2" -a -n "$vout_send_2" ]; then  # handle double input case
+        input="$input -i $txid_send_2:$vout_send_2"
+    fi
     _trace $send_cli fungible transfer \
         $blind_utxo_rcpt $amt_send $asset \
         $psbt $cons $disc $wtns \
-        -i $txid_send:$vout_send \
+        $input \
         -a $amt_change@$txid_change:$vout_change
     _subtit "waiting for witness psbt to appear"
     local tries=0
@@ -204,15 +228,12 @@ gen_blocks 103
 
 # asset issuance
 _tit 'issuing "USDT" asset'
-gen_utxo issuer
-txid_issue=$txid
-vout_issue=$vout
 issue_asset
 get_asset_id
 
 # asset transfer no. 1
 _tit 'transferring asset from issuer to recipient 1'
-transfer_asset issuer rcpt1 "$RGB0" "$RGB1" data0 data1 $txid_issue $vout_issue 1 100 900
+transfer_asset issuer rcpt1 "$RGB0" "$RGB1" data0 data1 $txid_issue $vout_issue 1 100 1900 $txid_issue_2 $vout_issue_2
 
 # asset transfer no. 2
 _tit 'transferring asset from recipient 1 to recipient 2'
